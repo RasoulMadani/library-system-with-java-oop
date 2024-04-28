4145902b9a098c52ea958c0aca139af181aed676
Rasoulmadani <ali.rahbary@chmail.ir>
2024-04-28 16:45:03 +0330

    add base library methods

---
+++ src/Borrow.java
@@ -0,0 +1,2 @@
+public class Borrow {
+}
---
+++ src/Library.java
@@ -0,0 +1,33 @@
+public class Library {
+    private String name;
+
+    public Library(String name){
+        this.name = name;
+    }
+
+    public void addMember(Member m){
+
+    }
+    public Member[] getMembers(){
+        return null;
+    }
+
+    public void addBook(Book b){
+
+    }
+    public Book[] getExistBook(){
+        return null;
+    }
+    public Book[] getBorrowedBooks(){
+        return  null;
+    }
+    public Book[] getAllBooks(){
+        return null;
+    }
+    public Borrow[] getBorrows(){
+        return null;
+    }
+    public Borrow getBorrow(int id){
+        return null;
+    }
+}

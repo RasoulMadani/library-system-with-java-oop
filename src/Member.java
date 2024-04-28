acf9d7f78ec973866abbfbbfd5fb6996eb9f4928
Rasoulmadani <ali.rahbary@chmail.ir>
2024-04-28 17:07:18 +0330

    create member class base methods

--- src/Member.java
+++ src/Member.java
@@ -1,2 +1,52 @@
 public class Member {
+    private String name;
+    private int ID;
+    private Address address;
+
+    public Member(String name, int ID) {
+        this.name = name;
+        this.ID = ID;
+    }
+
+    public Member(String name, int ID, Address address) {
+        this.name = name;
+        this.ID = ID;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+    public int getNumBorrows(){
+        return 0;
+    }
+    public void addBorrow(Borrow borrow){
+
+    }
+    public Borrow[] getTotalBorrows(){
+        return null;
+    }
+    public Member getMember(int id){
+        return null;
+    }
 }

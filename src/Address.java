0c834bf9aa0ba04b288b52213ad595d5a8c236f5
Rasoulmadani <ali.rahbary@chmail.ir>
2024-04-28 17:25:12 +0330

    create base mthods of writer and address class

--- src/Address.java
+++ src/Address.java
@@ -1,2 +1,53 @@
 public class Address {
+    private String provinceName, townName, streetName;
+    private int plaque;
+
+    public Address(String provinceName, String townName, String streetName, int plaque) {
+        this.provinceName = provinceName;
+        this.townName = townName;
+        this.streetName = streetName;
+        this.plaque = plaque;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getTownName() {
+        return townName;
+    }
+
+    public void setTownName(String townName) {
+        this.townName = townName;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public int getPlaque() {
+        return plaque;
+    }
+
+    public void setPlaque(int plaque) {
+        this.plaque = plaque;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "provinceName='" + provinceName + '\'' +
+                ", townName='" + townName + '\'' +
+                ", streetName='" + streetName + '\'' +
+                ", plaque=" + plaque +
+                '}';
+    }
 }
--- src/Writer.java
+++ src/Writer.java
@@ -1,2 +1,32 @@
 public class Writer {
+    private String firstName,lastName;
+
+    public Writer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Writer{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
